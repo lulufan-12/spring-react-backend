801d68aea933574e778848cc3028cfb102e77c89
Luis Martins <lulufan.12@gmail.com>
2021-06-29 16:46:10 -0300

    Corrected some logic on user controller

--- src/main/java/com/ultimate/springreact/model/repositories/ProjectRepository.java
+++ src/main/java/com/ultimate/springreact/model/repositories/ProjectRepository.java
@@ -1,9 +1,11 @@
 package com.ultimate.springreact.model.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ultimate.springreact.model.entities.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Integer> {
-
+	
 }
--- src/main/java/com/ultimate/springreact/model/repositories/UserProjectRepository.java
+++ src/main/java/com/ultimate/springreact/model/repositories/UserProjectRepository.java
@@ -1,10 +1,17 @@
 package com.ultimate.springreact.model.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ultimate.springreact.model.entities.UserProject;
 import com.ultimate.springreact.model.entities.UserProjectId;
 
 public interface UserProjectRepository extends CrudRepository<UserProject, UserProjectId> {
+	
+	
+	@Query("SELECT P FROM Project P, UserProject UP WHERE UP.userProjectId.userId = :userId AND P.id = UP.userProjectId.projectId")
+	public Iterable<UserProject> getUserProjects(@Param("userId") Integer userId);
+	
 	
 }
