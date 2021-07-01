565ea8bc143852620c6c1a712f362496b8fc7c69
Luis Martins <lulufan.12@gmail.com>
2021-07-01 20:58:55 -0300

    Refactoring of many classes Removed not used imports Removed business logic from controller Corrected all controllers to return a ResponseEntity Created a WorkedHoursService for the business logic of registering worked hours Also put all @Autowired into classes constructors.

--- src/main/java/com/ultimate/springreact/controllers/AdminController.java
+++ src/main/java/com/ultimate/springreact/controllers/AdminController.java
@@ -1,26 +1,27 @@
 package com.ultimate.springreact.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.ultimate.springreact.model.entities.WorkedHours;
 import com.ultimate.springreact.model.repositories.WorkedHoursRepository;
 
 @RestController
 @RequestMapping("/admin/api")
 public class AdminController {
+	private WorkedHoursRepository workedHoursRepository;
 	
 	@Autowired
-	WorkedHoursRepository workedHoursRepository;
-	
-	@GetMapping("/projects")
-	public Iterable<WorkedHours> getAllWorkedHours(){
-		
-		return workedHoursRepository.getTotalHoursByProject();
-		
+	public AdminController(WorkedHoursRepository workedHoursRepository) {
+		this.workedHoursRepository = workedHoursRepository;
 	}
 	
-	
-	
+	@GetMapping("/projects")
+	public ResponseEntity<Iterable<WorkedHours>> getAllWorkedHours(){	
+		
+		return ResponseEntity.ok(workedHoursRepository.getTotalHoursByProject());
+	}
 }
